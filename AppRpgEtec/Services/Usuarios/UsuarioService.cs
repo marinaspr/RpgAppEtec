30a56bcd84fc80b9aae3acc39aaea7c748030d64
unknown <hyejuaya@gmail.com>
2025-05-12 14:06:29 -0300

    inicio

---
+++ AppRpgEtec/Services/Usuarios/UsuarioService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Appointments.DataProvider;
+
+namespace AppRpgEtec.Services.Usuarios
+{
+    public class UsuarioService
+    {
+        public async Task<Usuario> PostRegistrarUsuarioAsync(Usuarios u)
+        {
+            string urlComplementar = "/Registrar";
+        }
+    }
+}
